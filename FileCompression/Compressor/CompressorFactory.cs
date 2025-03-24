d2a49b6ec31f330225392378affc69ff53e01b52
Nisim Kadosh <nisimkd1@gmail.com>
2025-03-25 00:04:58 +0200

    Add project files.

---
+++ FileCompression/Compressor/CompressorFactory.cs
@@ -0,0 +1,49 @@
+﻿using compress.model;
+using System.Reflection;
+
+namespace compress.Compressor
+{
+    public static class CompressorFactory
+    {
+        private static readonly Dictionary<string, ICompressor> _compressors = new();
+
+        static CompressorFactory()
+        {
+            LoadCompressors();
+        }
+
+        private static void LoadCompressors()
+        {
+            string directory = AppDomain.CurrentDomain.BaseDirectory;
+            string[] dllFiles = Directory.GetFiles(directory, "*.dll", SearchOption.TopDirectoryOnly);
+
+            foreach (string dll in dllFiles)
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(dll);
+                    var assemblyTypes = assembly.GetTypes()
+                        .Where(assemblyType => typeof(ICompressor).IsAssignableFrom(assemblyType) && !assemblyType.IsInterface);
+
+                    foreach (var assemblyType in assemblyTypes)
+                    {
+                        if (Activator.CreateInstance(assemblyType) is ICompressor instance)
+                        {
+                            _compressors[instance.GetName()] = instance;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to load compressors from {dll}: {ex.Message}");
+                }
+            }
+        }
+
+        public static ICompressor? GetCompressor(string type)
+        {
+            _compressors.TryGetValue(type, out var compressor);
+            return compressor;
+        }
+    }
+}
---
+++ FileCompression/Program.cs
@@ -0,0 +1,37 @@
+﻿using compress.Compressor;
+using compress.model;
+
+if (args.Length != 3)
+{
+    Console.WriteLine("Error: Invalid command-line arguments. Unable to compress the file.");
+    return;
+}
+
+string inputFilePath = args[0];
+
+if (!File.Exists(inputFilePath))
+{
+    Console.WriteLine($"Error: File '{inputFilePath}' does not exist.");
+    return;
+}
+
+string compressorType = args[1].ToLower();
+string outputFilePath = args[2];
+
+ICompressor? compressor = CompressorFactory.GetCompressor(compressorType);
+
+if (compressor == null)
+{
+    Console.WriteLine($"Error: Compression method '{compressorType}' is not supported.");
+    return;
+}
+
+try
+{
+    compressor.Compress(inputFilePath, outputFilePath);
+    Console.WriteLine($"File compressed successfully using {compressorType}!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Compression failed: {ex.Message}");
+}
